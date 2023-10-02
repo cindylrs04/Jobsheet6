0aabac1fe12d80e13cc98b0b83951c824b9abce8
cindylrs04 <larasaticindy231@gmail.com>
2023-10-02 12:10:04 +0700

    modifikasi 2

--- Pemilihan2Percobaan108.java
+++ Pemilihan2Percobaan108.java
@@ -1,22 +1,15 @@
 import java.util.Scanner;
 public class Pemilihan2Percobaan108 {
     public static void main(String[] args) {
-Scanner input08 = new Scanner (System.in);
-int tahun;
+ Scanner input24 = new Scanner(System.in);
 
-System.out.println("Masukkan tahun");
-tahun = input08.nextInt();
+        System.out.print("Masukkan Tahun : ");
+        int tahun = input24.nextInt();
 
-if ((tahun % 4) == 0) {
-    if ((tahun % 100) != 0) {
-    System.out.println("Tahun Kabisat");
-} else 
-    System.out.println("Bukan Tahun Kabisat");
-} else {
-    System.out.println("Bukan Tahun Kabisat");
-}
-
-    
-} 
-}
-    
+        if ((tahun % 4) == 0 && (!(tahun % 100  == 0) || (tahun % 400 == 0))) {
+                System.out.println("Tahun Kabisat");
+            } else {
+            System.out.println("Bukan Tahun Kabisat");
+            }
+        }
+    }
