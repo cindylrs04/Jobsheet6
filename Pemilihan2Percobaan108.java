b16e286fa2fbe82e66753f9b5ffede1241edc5d1
cindylrs04 <larasaticindy231@gmail.com>
2023-10-02 12:03:31 +0700

    Pemilihan2Percobaan108Modifikasi

--- Pemilihan2Percobaan108.java
+++ Pemilihan2Percobaan108.java
@@ -8,10 +8,15 @@ System.out.println("Masukkan tahun");
 tahun = input08.nextInt();
 
 if ((tahun % 4) == 0) {
-    if ((tahun % 100) != 0)
+    if ((tahun % 100) != 0) {
     System.out.println("Tahun Kabisat");
 } else 
     System.out.println("Bukan Tahun Kabisat");
+} else {
+    System.out.println("Bukan Tahun Kabisat");
+}
+
+    
+} 
 }
     
-}
