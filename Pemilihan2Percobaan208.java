c32b4d0e169d843d8a3c15ea14603f44ba793f18
cindylrs04 <larasaticindy231@gmail.com>
2023-10-02 12:50:48 +0700

    pemilihan2percobaan208modifikasi

--- Pemilihan2Percobaan208.java
+++ Pemilihan2Percobaan208.java
@@ -13,10 +13,13 @@ public class Pemilihan2Percobaan208 {
         totalSudut = sudut1 + sudut2 + sudut3;
 
         if(totalSudut == 180) {
-            if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else
-            System.out.println("Segitiga tersebut bukan segitiga siku-siku");
+            if((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            } else if ((sudut1 == sudut2 && sudut1 == sudut3 && sudut2 == sudut3)) {
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            } else if ((sudut1 == sudut2 || sudut1 == sudut3 || sudut2 == sudut3)) {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            }
 
             }
             else {
